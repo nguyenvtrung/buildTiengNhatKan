6a7aff72a4f6071d00ed1421bdcef069b2b3ce18
trung <nguyenvtrung7803@gmail.com>
2025-12-07 12:36:17 +0900

    update model ai

---
+++ assets/Breadcrumbs-TAlMKJjw.js
@@ -0,0 +1,74 @@
+var we=Object.defineProperty;var je=(e,t,o)=>t in e?we(e,t,{enumerable:!0,configurable:!0,writable:!0,value:o}):e[t]=o;var re=(e,t,o)=>je(e,typeof t!="symbol"?t+"":t,o);import{r as l,ak as ke,al as ve,am as Ve,R as q,an as ie,ao as Ie,o as P,j as b,p as Z,t as ee,ap as Le,w as z,ah as te,q as ce,M as ae,a3 as G,aq as le,y as pe,h as $e,z as De,ar as ue,a8 as Ne,k as ze}from"./index-CpKHcGHB.js";function Ue(e){if(e===void 0)throw new ReferenceError("this hasn't been initialised - super() hasn't been called");return e}function oe(e,t){var o=function(s){return t&&l.isValidElement(s)?t(s):s},i=Object.create(null);return e&&l.Children.map(e,function(n){return n}).forEach(function(n){i[n.key]=o(n)}),i}function Fe(e,t){e=e||{},t=t||{};function o(f){return f in t?t[f]:e[f]}var i=Object.create(null),n=[];for(var s in e)s in t?n.length&&(i[s]=n,n=[]):n.push(s);var r,p={};for(var u in t){if(i[u])for(r=0;r<i[u].length;r++){var d=i[u][r];p[i[u][r]]=o(d)}p[u]=o(u)}for(r=0;r<n.length;r++)p[n[r]]=o(n[r]);return p}function A(e,t,o){return o[t]!=null?o[t]:e.props[t]}function Ae(e,t){return oe(e.children,function(o){return l.cloneElement(o,{onExited:t.bind(null,o),in:!0,appear:A(o,"appear",e),enter:A(o,"enter",e),exit:A(o,"exit",e)})})}function Oe(e,t,o){var i=oe(e.children),n=Fe(t,i);return Object.keys(n).forEach(function(s){var r=n[s];if(l.isValidElement(r)){var p=s in t,u=s in i,d=t[s],f=l.isValidElement(d)&&!d.props.in;u&&(!p||f)?n[s]=l.cloneElement(r,{onExited:o.bind(null,r),in:!0,exit:A(r,"exit",e),enter:A(r,"enter",e)}):!u&&p&&!f?n[s]=l.cloneElement(r,{in:!1}):u&&p&&l.isValidElement(d)&&(n[s]=l.cloneElement(r,{onExited:o.bind(null,r),in:d.props.in,exit:A(r,"exit",e),enter:A(r,"enter",e)}))}}),n}var He=Object.values||function(e){return Object.keys(e).map(function(t){return e[t]})},We={component:"div",childFactory:function(t){return t}},ne=function(e){ke(t,e);function t(i,n){var s;s=e.call(this,i,n)||this;var r=s.handleExited.bind(Ue(s));return s.state={contextValue:{isMounting:!0},handleExited:r,firstRender:!0},s}var o=t.prototype;return o.componentDidMount=function(){this.mounted=!0,this.setState({contextValue:{isMounting:!1}})},o.componentWillUnmount=function(){this.mounted=!1},t.getDerivedStateFromProps=function(n,s){var r=s.children,p=s.handleExited,u=s.firstRender;return{children:u?Ae(n,p):Oe(n,r,p),firstRender:!1}},o.handleExited=function(n,s){var r=oe(this.props.children);n.key in r||(n.props.onExited&&n.props.onExited(s),this.mounted&&this.setState(function(p){var u=ve({},p.children);return delete u[n.key],{children:u}}))},o.render=function(){var n=this.props,s=n.component,r=n.childFactory,p=Ve(n,["component","childFactory"]),u=this.state.contextValue,d=He(this.state.children).map(r);return delete p.appear,delete p.enter,delete p.exit,s===null?q.createElement(ie.Provider,{value:u},d):q.createElement(ie.Provider,{value:u},q.createElement(s,p,d))},t}(q.Component);ne.propTypes={};ne.defaultProps=We;class _{constructor(){re(this,"mountEffect",()=>{this.shouldMount&&!this.didMount&&this.ref.current!==null&&(this.didMount=!0,this.mounted.resolve())});this.ref={current:null},this.mounted=null,this.didMount=!1,this.shouldMount=!1,this.setShouldMount=null}static create(){return new _}static use(){const t=Ie(_.create).current,[o,i]=l.useState(!1);return t.shouldMount=o,t.setShouldMount=i,l.useEffect(t.mountEffect,[o]),t}mount(){return this.mounted||(this.mounted=Ye(),this.shouldMount=!0,this.setShouldMount(this.shouldMount)),this.mounted}start(...t){this.mount().then(()=>{var o;return(o=this.ref.current)==null?void 0:o.start(...t)})}stop(...t){this.mount().then(()=>{var o;return(o=this.ref.current)==null?void 0:o.stop(...t)})}pulsate(...t){this.mount().then(()=>{var o;return(o=this.ref.current)==null?void 0:o.pulsate(...t)})}}function Xe(){return _.use()}function Ye(){let e,t;const o=new Promise((i,n)=>{e=i,t=n});return o.resolve=e,o.reject=t,o}function Ke(e){const{className:t,classes:o,pulsate:i=!1,rippleX:n,rippleY:s,rippleSize:r,in:p,onExited:u,timeout:d}=e,[f,h]=l.useState(!1),M=P(t,o.ripple,o.rippleVisible,i&&o.ripplePulsate),B={width:r,height:r,top:-(r/2)+s,left:-(r/2)+n},x=P(o.child,f&&o.childLeaving,i&&o.childPulsate);return!p&&!f&&h(!0),l.useEffect(()=>{if(!p&&u!=null){const w=setTimeout(u,d);return()=>{clearTimeout(w)}}},[u,p,d]),b.jsx("span",{className:M,style:B,children:b.jsx("span",{className:x})})}const E=Z("MuiTouchRipple",["root","ripple","rippleVisible","ripplePulsate","child","childLeaving","childPulsate"]),Q=550,qe=80,Ge=te`
+  0% {
+    transform: scale(0);
+    opacity: 0.1;
+  }
+
+  100% {
+    transform: scale(1);
+    opacity: 0.3;
+  }
+`,_e=te`
+  0% {
+    opacity: 1;
+  }
+
+  100% {
+    opacity: 0;
+  }
+`,Je=te`
+  0% {
+    transform: scale(1);
+  }
+
+  50% {
+    transform: scale(0.92);
+  }
+
+  100% {
+    transform: scale(1);
+  }
+`,Qe=z("span",{name:"MuiTouchRipple",slot:"Root"})({overflow:"hidden",pointerEvents:"none",position:"absolute",zIndex:0,top:0,right:0,bottom:0,left:0,borderRadius:"inherit"}),Ze=z(Ke,{name:"MuiTouchRipple",slot:"Ripple"})`
+  opacity: 0;
+  position: absolute;
+
+  &.${E.rippleVisible} {
+    opacity: 0.3;
+    transform: scale(1);
+    animation-name: ${Ge};
+    animation-duration: ${Q}ms;
+    animation-timing-function: ${({theme:e})=>e.transitions.easing.easeInOut};
+  }
+
+  &.${E.ripplePulsate} {
+    animation-duration: ${({theme:e})=>e.transitions.duration.shorter}ms;
+  }
+
+  & .${E.child} {
+    opacity: 1;
+    display: block;
+    width: 100%;
+    height: 100%;
+    border-radius: 50%;
+    background-color: currentColor;
+  }
+
+  & .${E.childLeaving} {
+    opacity: 0;
+    animation-name: ${_e};
+    animation-duration: ${Q}ms;
+    animation-timing-function: ${({theme:e})=>e.transitions.easing.easeInOut};
+  }
+
+  & .${E.childPulsate} {
+    position: absolute;
+    /* @noflip */
+    left: 0px;
+    top: 0;
+    animation-name: ${Je};
+    animation-duration: 2500ms;
+    animation-timing-function: ${({theme:e})=>e.transitions.easing.easeInOut};
+    animation-iteration-count: infinite;
+    animation-delay: 200ms;
+  }
+`,et=l.forwardRef(function(t,o){const i=ee({props:t,name:"MuiTouchRipple"}),{center:n=!1,classes:s={},className:r,...p}=i,[u,d]=l.useState([]),f=l.useRef(0),h=l.useRef(null);l.useEffect(()=>{h.current&&(h.current(),h.current=null)},[u]);const M=l.useRef(!1),B=Le(),x=l.useRef(null),w=l.useRef(null),T=l.useCallback(c=>{const{pulsate:C,rippleX:R,rippleY:m,rippleSize:S,cb:L}=c;d(j=>[...j,b.jsx(Ze,{classes:{ripple:P(s.ripple,E.ripple),rippleVisible:P(s.rippleVisible,E.rippleVisible),ripplePulsate:P(s.ripplePulsate,E.ripplePulsate),child:P(s.child,E.child),childLeaving:P(s.childLeaving,E.childLeaving),childPulsate:P(s.childPulsate,E.childPulsate)},timeout:Q,pulsate:C,rippleX:R,rippleY:m,rippleSize:S},f.current)]),f.current+=1,h.current=L},[s]),y=l.useCallback((c={},C={},R=()=>{})=>{const{pulsate:m=!1,center:S=n||C.pulsate,fakeElement:L=!1}=C;if((c==null?void 0:c.type)==="mousedown"&&M.current){M.current=!1;return}(c==null?void 0:c.type)==="touchstart"&&(M.current=!0);const j=L?null:w.current,$=j?j.getBoundingClientRect():{width:0,height:0,left:0,top:0};let D,v,N;if(S||c===void 0||c.clientX===0&&c.clientY===0||!c.clientX&&!c.touches)D=Math.round($.width/2),v=Math.round($.height/2);else{const{clientX:O,clientY:U}=c.touches&&c.touches.length>0?c.touches[0]:c;D=Math.round(O-$.left),v=Math.round(U-$.top)}if(S)N=Math.sqrt((2*$.width**2+$.height**2)/3),N%2===0&&(N+=1);else{const O=Math.max(Math.abs((j?j.clientWidth:0)-D),D)*2+2,U=Math.max(Math.abs((j?j.clientHeight:0)-v),v)*2+2;N=Math.sqrt(O**2+U**2)}c!=null&&c.touches?x.current===null&&(x.current=()=>{T({pulsate:m,rippleX:D,rippleY:v,rippleSize:N,cb:R})},B.start(qe,()=>{x.current&&(x.current(),x.current=null)})):T({pulsate:m,rippleX:D,rippleY:v,rippleSize:N,cb:R})},[n,T,B]),k=l.useCallback(()=>{y({},{pulsate:!0})},[y]),I=l.useCallback((c,C)=>{if(B.clear(),(c==null?void 0:c.type)==="touchend"&&x.current){x.current(),x.current=null,B.start(0,()=>{I(c,C)});return}x.current=null,d(R=>R.length>0?R.slice(1):R),h.current=C},[B]);return l.useImperativeHandle(o,()=>({pulsate:k,start:y,stop:I}),[k,y,I]),b.jsx(Qe,{className:P(E.root,s.root,r),ref:w,...p,children:b.jsx(ne,{component:null,exit:!0,children:u})})});function tt(e){return ce("MuiButtonBase",e)}const ot=Z("MuiButtonBase",["root","disabled","focusVisible"]),nt=e=>{const{disabled:t,focusVisible:o,focusVisibleClassName:i,classes:n}=e,r=pe({root:["root",t&&"disabled",o&&"focusVisible"]},tt,n);return o&&i&&(r.root+=` ${i}`),r},st=z("button",{name:"MuiButtonBase",slot:"Root",overridesResolver:(e,t)=>t.root})({display:"inline-flex",alignItems:"center",justifyContent:"center",position:"relative",boxSizing:"border-box",WebkitTapHighlightColor:"transparent",backgroundColor:"transparent",outline:0,border:0,margin:0,borderRadius:0,padding:0,cursor:"pointer",userSelect:"none",verticalAlign:"middle",MozAppearance:"none",WebkitAppearance:"none",textDecoration:"none",color:"inherit","&::-moz-focus-inner":{borderStyle:"none"},[`&.${ot.disabled}`]:{pointerEvents:"none",cursor:"default"},"@media print":{colorAdjust:"exact"}}),rt=l.forwardRef(function(t,o){const i=ee({props:t,name:"MuiButtonBase"}),{action:n,centerRipple:s=!1,children:r,className:p,component:u="button",disabled:d=!1,disableRipple:f=!1,disableTouchRipple:h=!1,focusRipple:M=!1,focusVisibleClassName:B,LinkComponent:x="a",onBlur:w,onClick:T,onContextMenu:y,onDragLeave:k,onFocus:I,onFocusVisible:c,onKeyDown:C,onKeyUp:R,onMouseDown:m,onMouseLeave:S,onMouseUp:L,onTouchEnd:j,onTouchMove:$,onTouchStart:D,tabIndex:v=0,TouchRippleProps:N,touchRippleRef:O,type:U,...H}=i,W=l.useRef(null),g=Xe(),de=ae(g.ref,O),[F,Y]=l.useState(!1);d&&F&&Y(!1),l.useImperativeHandle(n,()=>({focusVisible:()=>{Y(!0),W.current.focus()}}),[]);const fe=g.shouldMount&&!f&&!d;l.useEffect(()=>{F&&M&&!f&&g.pulsate()},[f,M,F,g]);const he=V(g,"start",m,h),me=V(g,"stop",y,h),ge=V(g,"stop",k,h),be=V(g,"stop",L,h),Me=V(g,"stop",a=>{F&&a.preventDefault(),S&&S(a)},h),xe=V(g,"start",D,h),Re=V(g,"stop",j,h),ye=V(g,"stop",$,h),Ce=V(g,"stop",a=>{le(a.target)||Y(!1),w&&w(a)},!1),Ee=G(a=>{W.current||(W.current=a.currentTarget),le(a.target)&&(Y(!0),c&&c(a)),I&&I(a)}),J=()=>{const a=W.current;return u&&u!=="button"&&!(a.tagName==="A"&&a.href)},Be=G(a=>{M&&!a.repeat&&F&&a.key===" "&&g.stop(a,()=>{g.start(a)}),a.target===a.currentTarget&&J()&&a.key===" "&&a.preventDefault(),C&&C(a),a.target===a.currentTarget&&J()&&a.key==="Enter"&&!d&&(a.preventDefault(),T&&T(a))}),Te=G(a=>{M&&a.key===" "&&F&&!a.defaultPrevented&&g.stop(a,()=>{g.pulsate(a)}),R&&R(a),T&&a.target===a.currentTarget&&J()&&a.key===" "&&!a.defaultPrevented&&T(a)});let K=u;K==="button"&&(H.href||H.to)&&(K=x);const X={};K==="button"?(X.type=U===void 0?"button":U,X.disabled=d):(!H.href&&!H.to&&(X.role="button"),d&&(X["aria-disabled"]=d));const Se=ae(o,W),se={...i,centerRipple:s,component:u,disabled:d,disableRipple:f,disableTouchRipple:h,focusRipple:M,tabIndex:v,focusVisible:F},Pe=nt(se);return b.jsxs(st,{as:K,className:P(Pe.root,p),ownerState:se,onBlur:Ce,onClick:T,onContextMenu:me,onFocus:Ee,onKeyDown:Be,onKeyUp:Te,onMouseDown:he,onMouseLeave:Me,onMouseUp:be,onDragLeave:ge,onTouchEnd:Re,onTouchMove:ye,onTouchStart:xe,ref:Se,tabIndex:d?-1:v,type:U,...X,...H,children:[r,fe?b.jsx(et,{ref:de,center:s,...N}):null]})});function V(e,t,o,i=!1){return G(n=>(o&&o(n),i||e[t](n),!0))}const it=$e(b.jsx("path",{d:"M6 10c-1.1 0-2 .9-2 2s.9 2 2 2 2-.9 2-2-.9-2-2-2zm12 0c-1.1 0-2 .9-2 2s.9 2 2 2 2-.9 2-2-.9-2-2-2zm-6 0c-1.1 0-2 .9-2 2s.9 2 2 2 2-.9 2-2-.9-2-2-2z"}),"MoreHoriz"),at=z(rt)(De(({theme:e})=>({display:"flex",marginLeft:`calc(${e.spacing(1)} * 0.5)`,marginRight:`calc(${e.spacing(1)} * 0.5)`,...e.palette.mode==="light"?{backgroundColor:e.palette.grey[100],color:e.palette.grey[700]}:{backgroundColor:e.palette.grey[700],color:e.palette.grey[100]},borderRadius:2,"&:hover, &:focus":{...e.palette.mode==="light"?{backgroundColor:e.palette.grey[200]}:{backgroundColor:e.palette.grey[600]}},"&:active":{boxShadow:e.shadows[0],...e.palette.mode==="light"?{backgroundColor:ue(e.palette.grey[200],.12)}:{backgroundColor:ue(e.palette.grey[600],.12)}}}))),lt=z(it)({width:24,height:16});function ut(e){const{slots:t={},slotProps:o={},...i}=e,n=e;return b.jsx("li",{children:b.jsx(at,{focusRipple:!0,...i,ownerState:n,children:b.jsx(lt,{as:t.CollapsedIcon,ownerState:n,...o.collapsedIcon})})})}function ct(e){return ce("MuiBreadcrumbs",e)}const pt=Z("MuiBreadcrumbs",["root","ol","li","separator"]),dt=e=>{const{classes:t}=e;return pe({root:["root"],li:["li"],ol:["ol"],separator:["separator"]},ct,t)},ft=z(ze,{name:"MuiBreadcrumbs",slot:"Root",overridesResolver:(e,t)=>[{[`& .${pt.li}`]:t.li},t.root]})({}),ht=z("ol",{name:"MuiBreadcrumbs",slot:"Ol",overridesResolver:(e,t)=>t.ol})({display:"flex",flexWrap:"wrap",alignItems:"center",padding:0,margin:0,listStyle:"none"}),mt=z("li",{name:"MuiBreadcrumbs",slot:"Separator",overridesResolver:(e,t)=>t.separator})({display:"flex",userSelect:"none",marginLeft:8,marginRight:8});function gt(e,t,o,i){return e.reduce((n,s,r)=>(r<e.length-1?n=n.concat(s,b.jsx(mt,{"aria-hidden":!0,className:t,ownerState:i,children:o},`separator-${r}`)):n.push(s),n),[])}const xt=l.forwardRef(function(t,o){const i=ee({props:t,name:"MuiBreadcrumbs"}),{children:n,className:s,component:r="nav",slots:p={},slotProps:u={},expandText:d="Show path",itemsAfterCollapse:f=1,itemsBeforeCollapse:h=1,maxItems:M=8,separator:B="/",...x}=i,[w,T]=l.useState(!1),y={...i,component:r,expanded:w,expandText:d,itemsAfterCollapse:f,itemsBeforeCollapse:h,maxItems:M,separator:B},k=dt(y),I=Ne({elementType:p.CollapsedIcon,externalSlotProps:u.collapsedIcon,ownerState:y}),c=l.useRef(null),C=m=>{const S=()=>{T(!0);const L=c.current.querySelector("a[href],button,[tabindex]");L&&L.focus()};return h+f>=m.length?m:[...m.slice(0,h),b.jsx(ut,{"aria-label":d,slots:{CollapsedIcon:p.CollapsedIcon},slotProps:{collapsedIcon:I},onClick:S},"ellipsis"),...m.slice(m.length-f,m.length)]},R=l.Children.toArray(n).filter(m=>l.isValidElement(m)).map((m,S)=>b.jsx("li",{className:k.li,children:m},`child-${S}`));return b.jsx(ft,{ref:o,component:r,color:"textSecondary",className:P(k.root,s),ownerState:y,...x,children:b.jsx(ht,{className:k.ol,ref:c,ownerState:y,children:gt(w||M&&R.length<=M?R:C(R),k.separator,B,y)})})});export{xt as B,rt as a};
